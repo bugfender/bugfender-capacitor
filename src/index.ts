415d13dd7afb5de8fadacae8be91c5de56a1c407
Asier Illarramendi <asier@illarra.com>
2022-07-14 18:27:58 +0200

    Update plugin loading logic so that in `web/electron` we directly load `@bugfender/sdk`

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
-import { registerPlugin } from '@capacitor/core';
-import type { BugfenderPlugin } from './definitions';
-import {BugfenderCapacitorWrapper} from "./bugfender-capacitor-wrapper";
+import type { BugfenderFacade } from '@bugfender/types';
+import { Capacitor, registerPlugin } from '@capacitor/core';
 
-const BugfenderCapacitor = registerPlugin<BugfenderPlugin>('Bugfender', {
-  web: () => import('./web').then(m => new m.BugfenderWeb()),
-  electron: () => import('./web').then(m => new m.BugfenderWeb()),
+import { BugfenderCapacitorWrapper } from './bugfender-capacitor-wrapper';
+import type { BugfenderPlugin } from './definitions';
+
+// Non-native platforms (web & electron) return directly the `Bugfender` instance from `@bugfender/sdk`.
+// For native we get a `BugfenderPlugin`.
+const BugfenderCapacitor = registerPlugin<BugfenderPlugin | BugfenderFacade>('Bugfender', {
+  web: () => import('@bugfender/sdk').then(m => m.Bugfender),
+  electron: () => import('@bugfender/sdk').then(m => m.Bugfender),
 });
 
-const Bugfender = new BugfenderCapacitorWrapper(BugfenderCapacitor)
+// eslint-disable-next-line import/no-mutable-exports
+let Bugfender: BugfenderFacade;
+
+if (Capacitor.isNativePlatform()) {
+  // Wrap `BugfenderPlugin` with a class that implements `BugfenderFacade` this is needed
+  // to homogeinize the user facing API. Capacitor is very limited & opinionated on how
+  // the plugin API should be.
+  Bugfender = new BugfenderCapacitorWrapper(BugfenderCapacitor as BugfenderPlugin);
+} else {
+  Bugfender = BugfenderCapacitor as BugfenderFacade;
+}
 
 export * from './definitions';
 export { Bugfender };
